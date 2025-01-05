1e0bab7dfb1c520f2c80f5ad1ea81f3d0642b707
Gabriel_Martins_Padoin <gabriel.padoin@woli.com.br>
2025-01-05 10:17:36 -0300

    [NG] EASIER_WAY_TO_SET_UP_CUSTOM_TWO-WAY_BINDING - refactor: implementing the model() function to set up the two-way binding property instead of staying with the manually set up version

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,13 +9,14 @@ import { Component, input, output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  size = input.required<{ width: string, height: string }>()
-  sizeChange = output<{ width: string, height: string }>()
+  // size = input.required<{ width: string, height: string }>()
+  // sizeChange = output<{ width: string, height: string }>()
+  size = model.required<{ width: string, height: string }>()
 
   onReset() {
-    this.sizeChange.emit({
+    this.size.update(() => ({
       width: '200',
       height: '100'
-    })
+    }))
   }
 }
